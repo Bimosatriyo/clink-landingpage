fbf7f55f6a729f6f4a69ffaabe678a21036bb51b
bimosatyrio <bimosatyrio@gmail.com>
2021-10-25 11:21:38 +0700

    initial commit

--- src/views/Index.js
+++ src/views/Index.js
@@ -19,57 +19,421 @@
 import React from "react";
 
 // reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
 // core components
-import IndexNavbar from "components/Navbars/IndexNavbar.js";
-import IndexHeader from "components/Headers/IndexHeader.js";
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import LandingPageHeader from "components/Headers/LandingPageHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
 
-// index sections
-import SectionButtons from "views/index-sections/SectionButtons.js";
-import SectionNavbars from "views/index-sections/SectionNavbars.js";
-import SectionNavigation from "views/index-sections/SectionNavigation.js";
-import SectionProgress from "views/index-sections/SectionProgress.js";
-import SectionNotifications from "views/index-sections/SectionNotifications.js";
-import SectionTypography from "views/index-sections/SectionTypography.js";
-import SectionJavaScript from "views/index-sections/SectionJavaScript.js";
-import SectionCarousel from "views/index-sections/SectionCarousel.js";
-import SectionNucleoIcons from "views/index-sections/SectionNucleoIcons.js";
-import SectionDark from "views/index-sections/SectionDark.js";
-import SectionLogin from "views/index-sections/SectionLogin.js";
-import SectionExamples from "views/index-sections/SectionExamples.js";
-import SectionDownload from "views/index-sections/SectionDownload.js";
-
-function Index() {
+function LandingPage() {
   document.documentElement.classList.remove("nav-open");
   React.useEffect(() => {
-    document.body.classList.add("index");
+    document.body.classList.add("profile-page");
     return function cleanup() {
-      document.body.classList.remove("index");
+      document.body.classList.remove("profile-page");
     };
   });
   return (
     <>
-      <IndexNavbar />
-      <IndexHeader />
+      <ExamplesNavbar />
+      <LandingPageHeader />
       <div className="main">
-        <SectionButtons />
-        <SectionNavbars />
-        <SectionNavigation />
-        <SectionProgress />
-        <SectionNotifications />
-        <SectionTypography />
-        <SectionJavaScript />
-        <SectionCarousel />
-        <SectionNucleoIcons />
-        <SectionDark />
-        <SectionLogin />
-        <SectionExamples />
-        <SectionDownload />
-        <DemoFooter />
+        <div className="section text-center">
+          <Container>
+            <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="title">So, What to do?</h2>
+                  <h5 className="description">
+                    Pelanggan menunggak? Its a big problem. Why? Karena ada hal yang harus kita tuntaskan.
+                  </h5>
+                <br />
+                {/* <Button
+                  className="btn-round"
+                  color="info"
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  See Details
+                </Button> */}
+              </Col>
+            </Row>
+            <br />
+            <br />
+            <Row>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <i className="nc-icon nc-money-coins" />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Abodemen</h4>
+                    <p className="description">
+                      Rupiah Abodemen yang disupport oleh tim CTB.
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <i className="nc-icon nc-settings" />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">NTE</h4>
+                    <p>
+                      NTE, yang diamankan dengan dismantle
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <i className="nc-icon nc-chart-bar-32" />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Lurus</h4>
+                    <p>
+                      Port ODP dan UIM, diluruskan dengan valins
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <i className="nc-icon nc-bulb-63" />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">OLT</h4>
+                    <p>
+                      Lalu bagaimana dengan port OLT? Proses yang terlewat ini menyebabkan ketidak lurusan data lapangan dan sistem di OLT
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section section-dark text-center">
+          <Container>
+            <h2 className="title">Let's talk about us</h2>
+            <Row>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={
+                          require("assets/img/faces/mba-rini.jpg")
+                            .default
+                        }
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Rini Wahyuni</CardTitle>
+                        <h6 className="card-category">Hustler</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      830025
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={
+                          require("assets/img/faces/tina.jpg").default
+                        }
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Agustina</CardTitle>
+                        <h6 className="card-category">Hipster</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      970027
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={
+                          require("assets/img/faces/mba-mariana.jpg")
+                            .default
+                        }
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Mariana</CardTitle>
+                        <h6 className="card-category">Hacker</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      890006
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section landing-section">
+          <Container>
+            <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="text-center">Keep in touch?</h2>
+                <Form className="contact-form">
+                  <Row>
+                    <Col md="6">
+                      <label>Name</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-single-02" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="Name" type="text" />
+                      </InputGroup>
+                    </Col>
+                    <Col md="6">
+                      <label>Email</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-email-85" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="Email" type="text" />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <label>Message</label>
+                  <Input
+                    placeholder="Tell us your thoughts and feelings..."
+                    type="textarea"
+                    rows="4"
+                  />
+                  <Row>
+                    <Col className="ml-auto mr-auto" md="4">
+                      <Button className="btn-fill" color="danger" size="lg">
+                        Send Message
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
+      <DemoFooter />
     </>
   );
 }
 
-export default Index;
+export default LandingPage;
+
+
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// import React from "react";
+
+// reactstrap components
+
+// core components
+// import IndexNavbar from "components/Navbars/IndexNavbar.js";
+// import IndexHeader from "components/Headers/IndexHeader.js";
+// import DemoFooter from "components/Footers/DemoFooter.js";
+
+// index sections
+// import SectionButtons from "views/index-sections/SectionButtons.js";
+// import SectionNavbars from "views/index-sections/SectionNavbars.js";
+// import SectionNavigation from "views/index-sections/SectionNavigation.js";
+// import SectionProgress from "views/index-sections/SectionProgress.js";
+// import SectionNotifications from "views/index-sections/SectionNotifications.js";
+// import SectionTypography from "views/index-sections/SectionTypography.js";
+// import SectionJavaScript from "views/index-sections/SectionJavaScript.js";
+// import SectionCarousel from "views/index-sections/SectionCarousel.js";
+// import SectionNucleoIcons from "views/index-sections/SectionNucleoIcons.js";
+// import SectionDark from "views/index-sections/SectionDark.js";
+// import SectionLogin from "views/index-sections/SectionLogin.js";
+// import SectionExamples from "views/index-sections/SectionExamples.js";
+// import SectionDownload from "views/index-sections/SectionDownload.js";
+
+// function Index() {
+//   document.documentElement.classList.remove("nav-open");
+//   React.useEffect(() => {
+//     document.body.classList.add("index");
+//     return function cleanup() {
+//       document.body.classList.remove("index");
+//     };
+//   });
+//   return (
+//     <>
+//       <IndexNavbar />
+//       <IndexHeader />
+//       <div className="main">
+//         <SectionButtons />
+//         <SectionNavbars />
+//         <SectionNavigation />
+//         <SectionProgress />
+//         <SectionNotifications />
+//         <SectionTypography />
+//         <SectionJavaScript />
+//         <SectionCarousel />
+//         <SectionNucleoIcons />
+//         <SectionDark />
+//         <SectionLogin />
+//         <SectionExamples />
+//         <SectionDownload />
+//         <DemoFooter />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Index;
+
